399ca0e3ea51e8d06241312a0937508f4f452bc8
takeyama <creative1902@gmail.com>
2022-05-05 00:04:46 +0900

    ✨ APIを叩いてみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,10 +3,23 @@ import styles from 'src/styles/Home.module.css'
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
 import { Main } from 'src/components/Main'
+import { useCallback, useEffect, useState } from 'react/'
 
 const Home = (props)=> {
 
-  const {foo, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd} = props;
+  const [posts,setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+  },[]);
+
+useEffect(()=>{
+  getPosts();
+},[]);
+
+  // const {foo, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd} = props;
 
   return (
     <div className={styles.container}>
@@ -16,22 +29,13 @@ const Home = (props)=> {
       </Head>
       <Header />
 
-      {isShow ? <h1>{foo}</h1> : null}
-      <button onClick={handleClick}>ボタン</button>
-      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
-
-      <input type="text" value={text} onChange={handleChange} />
-      <button onClick={handleAdd}>追加</button>
-      <ul>
-        {array.map((item)=>{
-          return <li key={item}>{item}</li>
-        })}
-      </ul>
-      
-      <Main page="index" />
-      <Footer />
+    <ol>
+      {posts.map((post)=>{
+        return <li key={post.id}>{post.title}</li>;
+      })}
+    </ol>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
