4d9e53bc5df45906a7b0f0543037ddc3e8563997
takeyama <creative1902@gmail.com>
2022-05-07 23:53:12 +0900

    ⚡️ useReducerを使いました

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,47 +1,98 @@
-import { useCallback, useEffect, useState } from 'react/'
+import { useCallback, useEffect, useReducer } from 'react/'
+
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
+
+const reducer = (state, action) =>{
+  switch(action.type){
+    case "end":{
+      return{
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+    }
+    case "error":{
+      return{
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+    }
+    default:{
+      throw new Error("no such action type!");
+    }
+  }
+};
 
  export const Posts = ()=> {
 
-  const [posts,setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  // const [posts,setPosts] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(null);
+
+  // const [state, setState] = useState({
+  //   data: [],
+  //   loading: true,
+  //   error: null,
+  // });
+
+const [state, dispatch] = useReducer(reducer, initialState);
 
   const getPosts = useCallback(async () => {
-
     try{
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-      if(!res.ok){
-        throw new Error("エラーが発生、データ取得失敗")
-      }
-
-      const json = await res.json();
-      setPosts(json);
-    }catch(error){
-      setError(error);
+        if(!res.ok){
+          throw new Error("エラーが発生、データ取得失敗")
+        }
+        const json = await res.json();
+        // setPosts(json);
+        // setState((prevState)=>{
+        //   return{
+        //     ...prevState,
+        //     data: json,
+        //     loading: false,
+        //   };
+        // });
+        dispatch({ type: "end", data: json});
     }
-    setLoading(false);
+    catch(error){
+        // setError(error);
+        // setState((prevState)=>{
+        //   return{
+        //     ...prevState,
+        //     loading: false,
+        //     error,
+        //   };
+        // });
+        dispatch({ type: "error", error});
+    }
+    // setLoading(false);
   },[]);
 
 useEffect(()=>{
   getPosts();
 },[]);
 
-if(loading){
+if(state.loading){
     return <div>ローディング中</div>
 }
 
-if(error){
-    return <div>{error.message}</div>;
+if(state.error){
+    return <div>{state.error.message}</div>;
 }
 
-if(posts.length === 0){
+if(state.data.length === 0){
     return <div>データは空です</div>;
 }
 
   return (
     <ol>
-    {posts.map((post)=>{
+    {state.data.map((post)=>{
         return <li key={post.id}>{post.title}</li>;
     })}
     </ol>
