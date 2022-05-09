2c3b6d232eca8c2b1e111a200decce082e2e480d
takeyama <creative1902@gmail.com>
2022-05-10 00:58:27 +0900

    ✨ ユーザーのfeach、feacherの共有化

--- src/hooks/usePosts.jsx
+++ src/hooks/usePosts.jsx
@@ -1,16 +1,5 @@
+import { fetcher } from 'src/utils/fetcher';
 import useSWR from 'swr';
-
-const fetcher = async (url) => {
-    const response = await fetch(url);
-  
-  if(!response.ok){
-    throw new Error("エラー発生、データ取得失敗");
-  }
-  
-  const json = await response.json();
-  return json;
-  
-  };
   
   export const usePosts = () =>{
     const { data, error } = useSWR("https://jsonplaceholder.typicode.com/posts", fetcher);
