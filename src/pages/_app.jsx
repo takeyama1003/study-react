62208550df0c3fe235d8f4fefc4ab9e35f9ad255
takeyama <creative1902@gmail.com>
2022-05-10 23:45:25 +0900

    🔥 不必要なコード、ファイル削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,21 +1,14 @@
 import Head from 'next/head'
 import 'src/styles/globals.css'
-import { usebgColor } from 'src/hooks/usebgColor'
-import { useCounter } from 'src/hooks/useCounter'
-import { useInputArray } from 'src/hooks/useInputArray'
 
 const MyApp = ({ Component, pageProps }) => {
-
-  const counter = useCounter();
-  const inputArray = useInputArray();
-  usebgColor();
 
   return(
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} {...counter} {...inputArray}/>
+      <Component {...pageProps} />
     </>
   ) 
 }
